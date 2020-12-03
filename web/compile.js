4045d0bb8e8dbd33b07444318472168fad7be862
Florian Imdahl <florian@wire.com>
2020-12-03 14:02:08 +0100

    feat: Add otr.proto

--- web/compile.js
+++ web/compile.js
@@ -21,14 +21,20 @@ const fs = require('fs');
 const path = require('path');
 const {pbjs, pbts} = require('protobufjs/cli');
 
-const dtsOutput = path.join(__dirname, 'Protobuf.d.ts');
-const jsOutput = path.join(__dirname, 'Protobuf.js');
-const protoBufferFile = path.join(__dirname, '..', 'proto', 'messages.proto');
+const protoDir = path.join(__dirname, '../proto');
 
-pbjs.main(['--target', 'static-module', '--wrap', 'commonjs', protoBufferFile], (error, output) => {
-  if (error) {
-    throw error;
-  }
-  fs.writeFileSync(jsOutput, output, {encoding: 'utf8'});
-  pbts.main(['--out', dtsOutput, '--no-comments', jsOutput]);
-});
+const protoFiles = fs.readdirSync(protoDir);
+
+for (const protoFileName of protoFiles) {
+  const protoBufferFile = path.join(__dirname, '../proto', protoFileName);
+  const dtsOutput = path.join(__dirname, protoFileName.replace(/\.proto$/, '.d.ts'));
+  const jsOutput = path.join(__dirname, protoFileName.replace(/\.proto$/, '.js'));
+
+  pbjs.main(['--target', 'static-module', '--wrap', 'commonjs', protoBufferFile], (error, output) => {
+    if (error) {
+      throw error;
+    }
+    fs.writeFileSync(jsOutput, output, {encoding: 'utf8'});
+    pbts.main(['--out', dtsOutput, '--no-comments', jsOutput]);
+  });
+}
