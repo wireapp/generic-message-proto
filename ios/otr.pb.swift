ec6d833f6bdc80dce098b4f7bb0ad30d3b3302cd
Christoph Aldrian <christoph.aldrian@wire.com>
2025-07-23 14:07:31 +0200

    revert changes

--- ios/otr.pb.swift
+++ ios/otr.pb.swift
@@ -25,9 +25,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see http://www.gnu.org/licenses/.
 
-public impo
-somediff
-rt Foundation
+public import Foundation
 public import SwiftProtobuf
 
 // If the compiler emits an error on this type, it is because this file
